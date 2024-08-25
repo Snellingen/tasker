f279e3e6f4ce55a94e3de6e614bbd564919c59f7
Matias Snellingen <matias@snellingen.com>
2024-08-25 14:55:48 +0200

    date on task card

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -3,11 +3,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
 import { Task } from '../../services/task.service';
 import { MatCheckbox } from '@angular/material/checkbox';
+import { DateToStringPipe } from '../../pipes/date-to-string.pipe';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [MatCardModule, MatRippleModule, MatCheckbox],
+  imports: [MatCardModule, MatRippleModule, MatCheckbox, DateToStringPipe, MatIconModule],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
