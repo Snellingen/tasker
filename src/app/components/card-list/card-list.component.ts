a4fc57b80a3cb4b75e0823b57448da35f0468178
Matias Snellingen <matias@snellingen.com>
2024-08-25 14:55:28 +0200

    card list virtualization

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,19 +1,31 @@
 import { Task } from '../../services/task.service';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TaskCardComponent } from '../task-card/task-card.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-card-list',
   standalone: true,
-  imports: [TaskCardComponent],
+  imports: [TaskCardComponent, ScrollingModule],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardListComponent {
+export class CardListComponent implements AfterViewInit {
 
   @Input() dataSource: Task[] | null = [];
-  ngOnInit() {
+  @ViewChild('wrapper', { static: true }) wrapper!: ElementRef;
+  @ViewChild('viewport', { static: true }) viewport!: ElementRef;
+
+  ngAfterViewInit() {
+    const wrapperHeight = this.wrapper.nativeElement.offsetHeight;
+    console.log('Wrapper height:', wrapperHeight);
+    this.viewport.nativeElement.style.height = `${wrapperHeight}px`;
   }
+
+  itemPixelSize = 62;
+  minBufferPx = 2 * this.itemPixelSize;
+  maxBufferPx = 2 * this.itemPixelSize;
 
 }
