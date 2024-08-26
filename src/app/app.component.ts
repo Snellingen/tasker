d91fa1d932e3c78df3ccd0a027bb422d79291327
Matias Snellingen <matias@snellingen.com>
2024-08-26 13:54:46 +0200

    add theme toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeSwitchComponent } from './components/theme-switch/theme-switch.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ThemeSwitchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
---
+++ src/app/components/theme-switch/theme-switch.component.ts
@@ -0,0 +1,23 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-theme-switch',
+  standalone: true,
+  imports: [MatSlideToggleModule],
+  templateUrl: './theme-switch.component.html',
+  styleUrl: './theme-switch.component.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class ThemeSwitchComponent {
+
+  onToggleChange(e: MatSlideToggleChange) {
+    console.log('Theme switch toggled:', e);
+    if (e.checked) {
+      document.documentElement.classList.add('dark-theme');
+    } else {
+      document.documentElement.classList.remove('dark-theme');
+    }
+  }
+
+}
